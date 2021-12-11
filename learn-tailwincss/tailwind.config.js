83984b054c43121daeebd23dd1114685a441a227
Minh Phuong Do <minhphuongdo2001@gmail.com>
2021-12-12 00:28:53 +0700

    learn more

--- learn-tailwincss/tailwind.config.js
+++ learn-tailwincss/tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
+  mode: 'jit',
+  content: [
+    './**/*.html',
+    './**/*.{js,jsx,ts,tsx,vue}',
+  ],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        35: '35px'
+      },
+      color: {
+        gray33: '#333'
+      }
+    },
   },
   plugins: [],
 }
